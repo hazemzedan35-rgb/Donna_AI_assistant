ff58000ad663d25fc9a2e02ef18ce90fe571ec53
Moamen <hazemzedan35@gmail.com>
2026-09-25 21:03:50 +0300

    write all comments to my whole code

--- donna_ai.py
+++ donna_ai.py
@@ -1,3 +1,4 @@
+# the tool that make my code can talk with the computer 
 import os
 
 # that means that I wanna use a tool that can read my .env file which containing my api secret code 
@@ -7,13 +8,20 @@ from dotenv import load_dotenv
 #genai = a helper that lets Python talk to Google's AI
 from google import genai
 
+# read my .env file and hide my api sectret key in a temprory place in my computer memory called an environmental variable
 load_dotenv()
 
+# open a connection to google and use my api_key as id      # go check this place that that is named as "GEMINI_API_KEY" and give me its value 
 client = genai.client(api_key=os.getenv("GEMINI_API_KEY"))
 
+# this mean use the connection that I got to generate an answer then store it in a variable called response
 response =  client.models.generate_content(
+    # the version of ai that will be used to generate the answer
     model="gemini-2.0-flash",
-    contents="Why is the sky blue?"
+
+    # my question 
+    contents="what is python"
 )
 
+# print the value that I stored it in text 
 print(response.text)
